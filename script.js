87c8edba4a388f04e73b6fc0a4256c9c63ff65c2
bpth8 <itmy3denis@gmail.com>
2025-07-20 21:14:39 +0300

    work with variables

---
+++ script.js
@@ -0,0 +1,11 @@
+let surName = 'Reaposov';
+let firstName = 'Denis';
+let patronymic = 'Alexandrovich';
+
+// если использовать несколько вызовов, как по заданию
+console.log(surName);
+console.log(firstName);
+console.log(patronymic);
+
+// если использовать один вызов со всеми переменными сразу
+console.log(`${surName} ${firstName} ${patronymic}`);
