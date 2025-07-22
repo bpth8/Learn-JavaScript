372c5cae818fb981637f50c6a9cfad31a5ad04ee
bpth8 <itmy3denis@gmail.com>
2025-07-22 22:08:06 +0300

    добавлен ввод фио с привествием

---
+++ script.js
@@ -0,0 +1,5 @@
+let surName = prompt('введите свою Фамилию')
+let firstName = prompt('введите своё Имя')
+let patronymic = prompt('введите своё Отчество')
+
+alert(`Приветствую ${surName} ${firstName} ${patronymic}!`)
