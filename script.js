f1a2f3c9bbc80cad3689d9d171fe4855897570bf
bpth8 <itmy3denis@gmail.com>
2025-07-29 22:21:58 +0300

    работа с числами

---
+++ script.js
@@ -0,0 +1,17 @@
+// для преобразования из строки в число, используем Number(), иначе будет "склеивание" строки, 
+// он также проверяет является ли число - числом или нет
+
+let numberOne = Number(prompt('Введите первое число'))
+let numberTwo = Number(prompt('Введите второе число'))
+
+let resultNumberPlus = numberOne + numberTwo
+let resultNumberMinus = numberOne - numberTwo
+let resultNumberTimes = numberOne * numberTwo
+let resultNumberSlash = numberOne / numberTwo
+
+alert((`
+  Сумма: ${resultNumberPlus} 
+  Разность: ${resultNumberMinus} 
+  Произведение: ${resultNumberTimes}
+  Частное: ${resultNumberSlash}
+`))
