604934163b0c65519d188c0c735734c8e0207ad1
bpth8 <itmy3denis@gmail.com>
2025-07-30 21:17:11 +0300

    learn string

---
+++ script.js
@@ -0,0 +1,22 @@
+// 1. базовые преобразования
+let string = 'Javascript is AWESOME';
+console.log(string.toUpperCase());
+
+// другой вариант, но без переменной не сможем использовать в дальнейшем
+console.log('Javascript is AWESOME'.toUpperCase());
+
+// 2. извлечение подстрок
+
+// 3. поиск и замена
+let searchAndReplace = 'Я люблю JavaScript! Я люблю это дело';
+console.log(searchAndReplace.replace('JavaScript', 'программирование').replaceAll('люблю', 'обожаю'));
+
+// 4. работа с разделителями
+
+let fruits = 'яблоки, груши, виноград';
+
+// 5. проверка содержимого
+
+// 6. маскирование данных
+
+// 7. анализ строки
