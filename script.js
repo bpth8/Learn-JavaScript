f85f4a649c0a6812d1926dfb59920c9c7114f456
bpth8 <itmy3denis@gmail.com>
2025-08-29 11:32:21 +0300

    object learn

---
+++ script.js
@@ -0,0 +1,114 @@
+//1.
+const objStudent = {
+  name: 'Давид',
+  age: 16,
+  grade: 4,
+  schoolClass: 10,
+}
+console.log(`Студент ${objStudent.name} учится в ${objStudent.schoolClass} классе и ему ${objStudent.age} лет.`);
+
+//2.
+objStudent.grade = 5;
+objStudent.hobby = 'Рисование';
+console.log(`${objStudent.name} получил ${objStudent.grade} по предмету ${objStudent.hobby}.`);
+
+//3.
+const objCar = {
+  brand: 'VW',
+  model: 'Passat',
+  year: 2020,
+};
+
+for (let key in objCar) {
+  console.log(objCar[key]);
+};
+
+//4 
+const objSettings = {
+  theme: 'dark',
+  volume: 80,
+}
+
+if (objSettings.theme, objSettings.volume) {
+  console.log(`${objSettings.theme}`)
+} else {
+  console.log('Свойство не найдено!')
+};
+
+//5
+const objMovies = [
+  {title: 'Инception', director: 'Christopher Nolan', year: 2010},
+  {title: 'The Matrix', director: 'Lana Wachowski', year: 1999},
+  {title: 'Interstellar', director: 'Christopher Nolan', year: 2014}
+];
+
+for (let i = 0; i < objMovies.length; i++) {
+  if (objMovies[i].year < 2005) {
+    console.log(objMovies[i]);
+  }
+};
+
+//6
+let count = 0;
+
+for (let b = 0; b < objMovies.length; b++) {
+  if (objMovies[b].director === 'Christopher Nolan') {
+    count++;
+  }
+}
+
+console.log("Кол-во фильмов Кристофера Нолана:", count);
+
+//level medium
+//1
+const objCart = [
+  { name: 'Книга', price: 15, quantity: 2 },
+  { name: 'Кофе', price: 25, quantity: 1 },
+  { name: 'Фонарик', price: 10, quantity: 3 }
+];
+
+let total = 0;
+
+for (let b = 0; b < objCart.length; b++) {
+  total += objCart[b].price * objCart[b].quantity;
+}
+
+console.log("Общая сумма корзины: " + total);
+
+//2
+const users = [
+  { id: 1, name: 'Alice', isOnline: true },
+  { id: 2, name: 'Bob', isOnline: false },
+  { id: 3, name: 'Charlie', isOnline: true }
+];
+
+for (const user of users) {
+  if (user.isOnline === true) {
+    console.log('(Найди и выведи первого пользователя)', user);
+    break;
+  }
+};
+
+//3
+const objAnimals = [
+  {name: 'Кот', type: 'домашнее'}, 
+  {name: 'Медведь', type: 'дикое'}, 
+  {name: 'Собака', type: 'домашнее'}, 
+  {name: 'Лиса', type: 'дикое'}
+];
+
+const domestic = [];
+const wild = [];
+
+for (const animal of objAnimals) {
+  if (animal.type === 'домашнее') {
+    domestic.push(animal);
+  } else {
+    wild.push(animal);
+  }
+};
+
+console.log('домашнее животные: ', domestic, "дикие животные: ", wild);
+
+//4
+const objPlayers = [{name: 'Анна', score: 85}, {name: 'Борис', score: 95}, {name: 'Вика', score: 76}]
