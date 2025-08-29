e5ec00a73016f7824f86fae9abfca9f239a0f46d
bpth8 <itmy3denis@gmail.com>
2025-08-29 14:20:53 +0300

    function learn

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-1212
+//1
+function greet(name) {
+  return `Привет, ${name}`
+};
+console.log(greet(prompt("Введите свое имя:", '')));
+
+//2
+function sum(a, b) {
+  a = 5;
+  b = 11;
+  return a + b;
+};
+console.log(sum());
