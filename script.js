20b566cbba31c7c1e96e97b762612e924e909f1b
bpth8 <itmy3denis@gmail.com>
2025-09-30 15:17:02 +0300

    funct2

--- script.js
+++ script.js
@@ -49,4 +49,60 @@ const countProperties = (objBrand) => {
 console.log(countProperties(objBrand));
 
 //5
+let scores = {
+	Alex: 100,
+	Bob: 75,
+	Charlie: 95
+};
+
+let values = Object.values(scores);
+let max = Math.max(...values);
+console.log("максимальное значение:", max);
+
+//6
+const objAuto = {
+	Brand: "Volkswagen",
+	Year: 2020,
+	Model: 'Touareg',
+	engineDisplacement: 3000,
+	Color: 'Black',
+	Fuel: 'Diesel',
+	Weight: 3200
+};
+
+function sumNumValues(objAuto) {
+	const values = Object.values(objAuto);
+	let sum = 0;
+
+	for (let value of values) {
+		if (typeof value === 'number') {
+			sum += value;
+		}
+	}
+
+	return sum;
+};
+
+console.log(sumNumValues(objAuto));
+
+//Сравнение двух объектов
+//тут какой нибудь obj1 и obj2..
+
+function areObjectsEqual(obj1, obj2) {
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  };
+
+  for (let key of keys1) {
+    if (obj1[key] !== obj2[key]) {
+      return false;
+    }
+  };
+
+  return true;
+};
+
 
