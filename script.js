160ecdfc0d830708d4e805a197711bbda65aeab6
bpth8 <itmy3denis@gmail.com>
2025-10-22 15:53:26 +0300

    learn methods array.map()

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ import { fruitLenght, keysName, newNum } from './array/arrayMap.js';
 import './array/arrayFilter.js';
 import './array/arrayReduce.js';
 
-//=== arrayMap===
-
-//1
-console.log(newNum);
-
-//2 
-console.log(fruitLenght);
-
-//3
-console.log(keysName);
+/*1 */ console.log(newNum);
+/*2 */ console.log(fruitLenght);
+/*3 */ console.log(keysName);
+/*4 */
+/*5 */
+/*6 */
+/*7 */
+/*8 */
+/*9 */
