16a3c5c00b2740207f75f1916e9baf19a1892713
bpth8 <itmy3denis@gmail.com>
2025-10-22 08:55:12 +0300

    learn array-methods

---
+++ arrayMap.js
@@ -0,0 +1,7 @@
+//1
+const numMassive = [1, 2, 3, 4, 5];
+const newNum = numMassive.map((a) => a *2);
+export { newNum };
+
+//2
+
--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-///
+import { newNum } from './arrayMap.js';
+import './arrayFilter.js';
+import './arrayReduce.js';
+
+//1
+console.log(newNum);
