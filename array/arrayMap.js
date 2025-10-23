8a940dace962e3ce887fc77e4da3e911b7d42e1f
bpth8 <itmy3denis@gmail.com>
2025-10-23 10:33:12 +0300

    arrayMap

--- array/arrayMap.js
+++ array/arrayMap.js
@@ -21,4 +21,39 @@ const objMassive = [
 const keysName = objMassive.map((item) => item.name);
 
 //4
-const temperatur = [0, 15, 25, 30, -5];
+export { fahrenheit };
+
+const temperatures = [0, 15, 25, 30, -5];
+const fahrenheit = temperatures.map((item) => item *9/5 +32);
+
+//5 условие задачи вообще не понял. 
+
+//6
+export { capitalizedString };
+
+const stringArray = ["hello", "world", "javascript"];
+const capitalizedString = stringArray.map((item) => item.charAt(0).toUpperCase() + item.slice(1));
+
+//7
+export { updatedNumbers };
+
+const numberArray = [1, 2, 3, 4, 5];
+const updatedNumbers = numberArray.map((num) => { return num % 2 === 0 ? num * 2 : num });
+
+//8
+export { stringToNumberConversion };
+
+const arrayNumbers = [1, "2", 3, "4", 5];
+const stringToNumberConversion = arrayNumbers.map((num) => Number(num));
+
+//9
+export { discountedProducts };
+
+const products = [
+  {name: "apple", price: 1.2}, 
+  {name: "banana", price: 0.8}
+];
+
+const discountedProducts = products.map(product => 
+  ({...product, discountedPrice: + (product.price * 0.9).toFixed(2)})
+);
--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-import { fruitLenght, keysName, newNum } from './array/arrayMap.js';
+import {
+  discountedProducts,
+  stringToNumberConversion, 
+  updatedNumbers, 
+  capitalizedString, 
+  fahrenheit, 
+  fruitLenght, 
+  keysName, 
+  newNum } from './array/arrayMap.js';
+
 import './array/arrayFilter.js';
 import './array/arrayReduce.js';
 
+console.log('Array.map()')
 /*1 */ console.log(newNum);
 /*2 */ console.log(fruitLenght);
 /*3 */ console.log(keysName);
-/*4 */
+/*4 */ console.log(fahrenheit);
 /*5 */
-/*6 */
-/*7 */
-/*8 */
-/*9 */
+/*6 */ console.log(capitalizedString);
+/*7 */ console.log(updatedNumbers);
+/*8 */ console.log(stringToNumberConversion)
+/*9 */ console.log(discountedProducts);
