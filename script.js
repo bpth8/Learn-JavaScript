ed4af10cc193374837000d3aa21dd0193afd8369
bpth8 <itmy3denis@gmail.com>
2025-12-06 12:32:38 +0200

    develop

---
+++ script.js
@@ -0,0 +1,115 @@
+const DOM = {
+
+  type: 'body',
+
+  classNames: [],
+
+  id: null,
+
+  children: [
+
+    {
+
+      type: 'div',
+
+      classNames: ['wrapper'],
+
+      id: null,
+
+      children: [
+
+        {
+
+          type: 'header',
+
+          classNames: ['header', 'container', 'mb-8'],
+
+          id: null,
+
+          children: [],
+
+        },
+
+        {
+
+          type: 'div',
+
+          classNames: ['main', 'container', 'mb-8'],
+
+          id: null,
+
+          children: [
+
+            {
+
+              type: 'div',
+
+              classNames: ['products', 'mb-8'],
+
+              id: 'unicalId',
+
+              children: [
+
+                {
+
+                  type: 'div',
+
+                  classNames: ['product'],
+
+                  id: null,
+
+                  children: [],
+
+                },
+
+                {
+
+                  type: 'div',
+
+                  classNames: ['product'],
+
+                  id: null,
+
+                  children: [],
+
+                },
+
+                {
+
+                  type: 'div',
+
+                  classNames: ['product'],
+
+                  id: null,
+
+                  children: [],
+
+                },
+
+              ],
+
+            },
+
+          ],
+
+        },
+
+        {
+
+          type: 'footer',
+
+          classNames: ['footer', 'container'],
+
+          id: null,
+
+          children: [],
+
+        },
+
+      ],
+
+    },
+
+  ],
+
+};
